99d3b16775e7f41b4c7e3206de5c5b607432cd71
brcOne <amv.0280@gmail.com>
2025-03-26 10:32:12 +0000

    sql error

--- resolution_des_exception/database/migrations/2025_03_26_100556_create_articles_table.php
+++ resolution_des_exception/database/migrations/2025_03_26_100556_create_articles_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
-            $table->string('title')->unique(); // Make title unique
+            $table->string('title')->unique(); 
            
             $table->timestamps();
         });
